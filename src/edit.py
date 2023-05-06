2f05cc0fe56ce66ef85f7502ac54baac32e2f9db
Ravidu Perera <sheahn20133@gmail.com>
2023-05-06 14:22:53 +0530

    Revert "add grafana"
    This reverts commit d646a0874374d162f9d6357f9bbdf0ebab1deffe.

--- src/edit.py
+++ src/edit.py
@@ -1,17 +1,19 @@
 from datetime import datetime
 import os
-import time
 from flask import Flask, request, jsonify
 import openai
 from google.cloud import firestore
 import firebase_admin
 from firebase_admin import credentials
-from prometheus_client import Counter, Gauge, start_http_server
 
-# Setup Firebase credentials
+
+
+#setup firebase credentials
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 database = os.environ.get('FIREBASE_DATABASE_COLLECTION')
+
+app = Flask(__name__)
 
 # Load OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
@@ -20,18 +22,8 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 project_id = "apiservices-384019"
 db = firestore.Client(project=project_id)
 
-# Define metrics
-requests_total = Counter('requests_total', 'Total number of requests')
-request_duration_seconds = Gauge('request_duration_seconds', 'Duration of request in seconds')
-
-app = Flask(__name__)
-
 @app.route('/')
 def edit():
-    # Increment request counter and start timing request
-    requests_total.inc()
-    start_time = time.time()
-
     # Add service name, status, and timestamp to Firestore
     service_ref = db.collection(database).document('EditService_Status')
     print("Edit Service starting...")
@@ -55,8 +47,6 @@ def edit():
             stop=None,
             temperature=0.5
         )
-        # Record request duration and return response
-        request_duration_seconds.observe(time.time() - start_time)
         return jsonify(response=response.choices[0].text.strip())
     except Exception as e:
         # Update service status to 'error' if an exception occur
@@ -64,8 +54,4 @@ def edit():
         return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
-    # Start HTTP server for metrics exposition
-    start_http_server(8000)
-
-    # Start Flask app
     app.run(debug=True, host='0.0.0.0', port=5003)
